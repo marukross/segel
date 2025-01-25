a18a33b954f1fe579b85d140d4cf16826bb624cf
marcos bogarin <marcosbogarincano@gmail.com>
2025-01-25 13:55:29 -0300

    se configura init, manifest en el root de modulo y se aplica ajuste minimos a sintaxis en init de model

---
+++ addons/venta/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Extension Sale Segel',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Se exitende la funcionalidad el modulo sale',
+    'depends': ['sale', 'account'],
+    'data': [
+        'views/sale_order_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+}
