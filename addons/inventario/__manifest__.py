0112b0a73f52737df92387b3940564a3670b03c4
marcos bogarin <marcosbogarincano@gmail.com>
2025-01-25 10:00:08 -0300

    se modifica manifiesto

--- addons/inventario/__manifest__.py
+++ addons/inventario/__manifest__.py
@@ -4,6 +4,10 @@
     'category': 'Inventory',
     'summary': 'Modulo de la prueba tecnica para Segel',
     'depends': ['stock'],
+    'data': [
+        'views/producto_personalizado_views.xml',
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
     'application': True,
 }
