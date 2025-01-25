20c4cd9315ccebadcf02dd1cec31cbf467a4743c
marcos bogarin <marcosbogarincano@gmail.com>
2025-01-25 09:47:43 -0300

    estructura base para modulo personalizado

---
+++ addons/inventario/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Productos Personalizados Segel',
+    'version': '1.0',
+    'category': 'Inventory',
+    'summary': 'Modulo de la prueba tecnica para Segel',
+    'depends': ['stock'],
+    'installable': True,
+    'application': True,
+}
